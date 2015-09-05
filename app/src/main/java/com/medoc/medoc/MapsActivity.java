2705750b220372f54fb0fbf507953910cc02092b
Alexandre Erich Sébastien Georges <erichaitian97@gmail.com>
2015-09-05 07:40:05 -0400

    MeDOC

---
+++ app/src/main/java/com/medoc/medoc/MapsActivity.java
@@ -0,0 +1,30 @@
+package com.medoc.medoc;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng pap = new LatLng(18.5790371, -72.28446);
+        map.addMarker(new MarkerOptions().position(pap).title("Hello Ayiti!"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(pap));
+    }
+}
