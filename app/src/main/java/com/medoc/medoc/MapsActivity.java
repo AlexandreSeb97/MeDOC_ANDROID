609a508dc0d04a995d5a0b678783610438b791fe
Alexandre Erich Sébastien Georges <erichaitian97@gmail.com>
2015-09-09 05:30:14 -0400

    New Hospitals

--- app/src/main/java/com/medoc/medoc/MapsActivity.java
+++ app/src/main/java/com/medoc/medoc/MapsActivity.java
@@ -6,7 +6,10 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -24,10 +27,33 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap map) {
         // Add a marker in Sydney, Australia, and move the camera.
         LatLng pap = new LatLng(18.5790371, -72.28446);
+        map.setMyLocationEnabled(true);
         map.addMarker(new MarkerOptions().position(pap).title("Hello Ayiti!"));
         map.addMarker(new MarkerOptions()
-                .position(new LatLng(18.5543128, -72.2999572))
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
+                .snippet("Public - Universitaire")
+                .position(new LatLng(18.556353, -72.298126))
                 .title("Hopital Universitaire de la Paix"));
+        map.addMarker(new MarkerOptions()
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
+                .snippet("Privé")
+                .position(new LatLng(18.533449, -72.316445))
+                .title("Hopital du Canapé-Vert"));
+        map.addMarker(new MarkerOptions()
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
+                .snippet("Public - Universitaire")
+                .position(new LatLng(18.5393845, -72.3401777))
+                .title("Hopital de l'Université d'Etat d'Haiti"));
+        map.addMarker(new MarkerOptions()
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
+                .snippet("Public")
+                .position(new LatLng(18.538308, -72.3438775))
+                .title("Hopital St Francois de Sales"));
+        map.addMarker(new MarkerOptions()
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
+                .snippet("Privé")
+                .position(new LatLng(18.5373134, -72.3456348))
+                .title("Asile Francais"));
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(pap, 13));
     }
 }
